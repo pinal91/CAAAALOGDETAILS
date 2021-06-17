420cf329b79edd4e9265235ae2367684299ff0b3
patelpin <pinal3291@gmail.com>
2021-06-17 14:04:29 +0530

    Initial commit

---
+++ app/src/main/java/net/pinal/firebase/ArtistActivity.java
@@ -0,0 +1,205 @@
+package net.pinal.firebase;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArtistActivity extends AppCompatActivity {
+
+    Button buttonAddTrack;
+    EditText editTextTrackName;
+    SeekBar seekBarRating;
+    TextView textViewRating, textViewArtist;
+    ListView listViewTracks;
+
+    DatabaseReference databaseTracks;
+
+    List<Track> tracks;
+    String Artist_ID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artist);
+
+        Intent intent = getIntent();
+
+        /*
+         * this line is important
+         * this time we are not getting the reference of a direct node
+         * but inside the node track we are creating a new child with the artist id
+         * and inside that node we will store all the tracks with unique ids
+         * */
+
+        Artist_ID=intent.getStringExtra(MainActivity.ARTIST_ID);
+        databaseTracks = FirebaseDatabase.getInstance().getReference("tracks").
+                child(Artist_ID);
+
+        buttonAddTrack = (Button) findViewById(R.id.buttonAddTrack);
+        editTextTrackName = (EditText) findViewById(R.id.editTextName);
+        seekBarRating = (SeekBar) findViewById(R.id.seekBarRating);
+        textViewRating = (TextView) findViewById(R.id.textViewRating);
+        textViewArtist = (TextView) findViewById(R.id.textViewArtist);
+        listViewTracks = (ListView) findViewById(R.id.listViewTracks);
+
+        tracks = new ArrayList<>();
+
+        textViewArtist.setText(intent.getStringExtra(MainActivity.ARTIST_NAME));
+
+        seekBarRating.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textViewRating.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        buttonAddTrack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveTrack();
+            }
+        });
+
+        listViewTracks.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Track artist = tracks.get(i);
+                showUpdateDeleteDialog(artist.getId(), artist.getTrackName(),artist.getRating());
+                return true;
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseTracks.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                tracks.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Track track = postSnapshot.getValue(Track.class);
+                    tracks.add(track);
+                }
+                TrackList trackListAdapter = new TrackList(ArtistActivity.this, tracks);
+                listViewTracks.setAdapter(trackListAdapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void saveTrack() {
+        String trackName = editTextTrackName.getText().toString().trim();
+        int rating = seekBarRating.getProgress();
+        if (!TextUtils.isEmpty(trackName)) {
+            String id  = databaseTracks.push().getKey();
+            Track track = new Track(id, trackName, rating);
+            databaseTracks.child(id).setValue(track);
+            Toast.makeText(this, "Track saved", Toast.LENGTH_LONG).show();
+            editTextTrackName.setText("");
+        } else {
+            Toast.makeText(this, "Please enter track name", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean updateArtist(String id, String name, int rating) {
+        //getting the specified artist reference
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("tracks"). child(Artist_ID);;
+
+        //updating artist
+        Track artist = new Track(id, name, rating);
+        dR.child(id).setValue(artist);
+        Toast.makeText(getApplicationContext(), "Track Updated", Toast.LENGTH_LONG).show();
+        return true;
+    }
+
+    private boolean deleteArtist(String id) {
+        //getting the specified artist reference
+        DatabaseReference dR = FirebaseDatabase.getInstance().getReference("tracks"). child(Artist_ID);;
+
+        //removing artist
+        dR.child(id).removeValue();
+
+        Toast.makeText(getApplicationContext(), "Track Deleted", Toast.LENGTH_LONG).show();
+
+        return true;
+    }
+
+    private void showUpdateDeleteDialog(final String artistId, String artistName, final int rating) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextName = (EditText) dialogView.findViewById(R.id.editTextName);
+        final Spinner spinnerGenre = (Spinner) dialogView.findViewById(R.id.spinnerGenres);
+        final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateArtist);
+        final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDeleteArtist);
+
+        spinnerGenre.setVisibility(View.GONE);
+
+        dialogBuilder.setTitle(artistName);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString().trim();
+                if (!TextUtils.isEmpty(name)) {
+                    updateArtist(artistId, name,rating);
+                    b.dismiss();
+                }
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               deleteArtist(artistId);
+                b.dismiss();
+            }
+        });
+    }
+
+
+}
